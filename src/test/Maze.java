5f12b5b6983325e63489976b40a83a1b773d03e4
28kayak <kaya_ota@msn.com>
2014-11-03 02:37:32 -0800

    init

---
+++ src/test/Maze.java
@@ -0,0 +1,151 @@
+package test;
+
+//import java.awt.Color;
+//import java.awt.Dimension;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.SWT;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+
+public class Maze {
+
+	protected Shell shell;
+	int height;
+	int width;
+	int matrixSize = 6;
+	int index = 0;
+	final Label[] wallw = new Label[36];
+	final Label[] wallh = new Label[36];
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Maze window = new Maze();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(600, 600);
+		shell.setText("SWT Application");
+		shell.setLayout(null);
+		height = 4;
+		width = 4;
+		//Dimension wallDim = new Dimension(150,20);
+		
+		for(int k = 0; k < matrixSize; k++)
+		{
+			for(int i = 0; i < matrixSize; i++)
+			{
+				index = i + matrixSize * k;
+				//System.out.println(index);
+				wallw[index]=new Label(shell, SWT.None);
+				wallw[index].setText(Integer.toString(index));
+				//set Colors
+				wallw[index].setBackground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
+				wallw[index].setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
+				//set Locations
+				wallw[index].setBounds(150*k, 150*i, 150, 20);
+				wallw[index].addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseDown(MouseEvent e)
+					{
+						String mouseE = e.toString();
+						System.out.println(mouseE);
+						String[] info = mouseE.split(" ");
+						System.out.println(info[1]);
+						String deta[] = info[1].split("\\{");
+						String deta1[] = deta[1].split("\\}");
+						System.out.println("deta = "+ deta[1]);
+						System.out.println("deta1 = "+ deta1[0]);
+						int newIndex = Integer.parseInt(deta1[0]);
+					
+						
+						System.out.println("in wallw, index = " + index);
+						System.out.println("before if wallw");
+						if(wallw[newIndex].getBackground().equals(SWTResourceManager.getColor(SWT.COLOR_BLACK)))
+						{	
+							System.out.println("in if wallw");
+							wallw[newIndex].setBackground(SWTResourceManager.getColor(SWT.COLOR_DARK_MAGENTA));
+						}
+						else
+						{
+							System.out.println("in else wallw");
+							wallw[newIndex].setBackground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
+						}
+					}
+				});//addMouseListener
+			}//for-i
+		}//for-k
+		for(int k = 0; k < matrixSize; k++)
+		{
+			for(int i = 0; i < matrixSize; i++)
+			{
+				index = i + matrixSize*k;
+				wallh[index] = new Label(shell, SWT.None);
+				wallh[index].setText(Integer.toString(index));
+				wallh[index].setBackground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
+				wallh[index].setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
+				wallh[index].setBounds(150*k, 150*i, 20, 150);
+				wallh[index].addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseDown(MouseEvent e)
+					{
+						String mouseE = e.toString();
+						System.out.println(mouseE);
+						String[] info = mouseE.split(" ");
+						System.out.println(info[1]);
+						String deta[] = info[1].split("\\{");
+						String deta1[] = deta[1].split("\\}");
+						System.out.println("deta = "+ deta[1]);
+						System.out.println("deta1 = "+ deta1[0]);
+						int newIndex = Integer.parseInt(deta1[0]);
+					
+						
+						System.out.println("in wallw, index = " + index);
+						System.out.println("before if wallw");
+						if(wallh[newIndex].getBackground().equals(SWTResourceManager.getColor(SWT.COLOR_BLACK)))
+						{	
+							System.out.println("in if wallw");
+							wallh[newIndex].setBackground(SWTResourceManager.getColor(SWT.COLOR_DARK_MAGENTA));
+						}
+						else
+						{
+							System.out.println("in else wallw");
+							wallh[newIndex].setBackground(SWTResourceManager.getColor(SWT.COLOR_BLACK));
+						}
+					}
+				});//addMouseListener
+				
+			}//for i
+		}//for k
+	}
+}
