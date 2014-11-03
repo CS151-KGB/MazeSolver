75809b27d835dde46694c5d259312ea6f78d7320
28kayak <kaya_ota@msn.com>
2014-11-03 02:53:45 -0800

    edit

--- src/test/Robot.java
+++ src/test/Robot.java
@@ -1,5 +1,62 @@
 package test;
 
-public class Robot {
+public class Robot 
+{
+	enum directions{front, right, left, back,}
+	//enum wall{oepned, closed,}
+	
+	int getCurrentRoomID(int current_x, int current_y)
+	{
+		int current_id;
+		current_id = current_x + 4*current_y;
+		return current_id;
+	}
+	boolean[] getSensorValues()
+	{//index represents the direction where the sensor supposes to be.
+	// value corresponding index represents the detection of the walls.
+	// if there is a wall, return t; otherwise, false
+	// if door is opened, hold value is true; otherwise, false.
+	//      1
+	//3  (Robot)  2
+	//      4
+		boolean sensorResult[] = new boolean[4];//Not use sensorResult[0] for intuitive
+		
+		 
+		  
+		return sensorResult;
+	}
+	void setCurrentRoom()
+	{//assume the instances of rooms has been allocated.
+		boolean[] sensorValues = getSensorValues();
+		//int currentRoomId = getCurrentRoomID();
+		if(sensorValues[1] == true)
+		{//if the head of robot is true(opened)
+			//Room currentRoom = new Room();
+			
+			
+			
+			
+		}
+		else if(sensorValues[2] == true)
+		{
+			
+		}
+		else if(sensorValues[3] == true)
+		{
+			
+		}
+		else
+		{
+			//something goes wrong.
+		}
+		
+		
+		
+		
+	}//setCurrentRoom
+	
+	
+}//class
 
-}
+
+
--- src/test/Room.java
+++ src/test/Room.java
@@ -1,5 +1,102 @@
 package test;
 
-public class Room {
+//import Room;
 
+import java.awt.Color;
+public class Room
+{
+	int locationx;
+	int locationy;
+	int roomId;
+	Room north;
+	Room east;
+	Room south;
+	Room west;
+	Color floorCol;
+	public Room()
+	{
+		
+	}
+	void setLocx(int locationx)
+	{
+		this.locationx = locationx;
+	}
+	void setLocy(int locationy)
+	{
+		this.locationy = locationy;
+	}
+	void setNorthRoom(Room north)
+	{
+		this.north = north;
+	}
+	void setEastRoom(Room east)
+	{
+		this.east = east;
+	}
+	void setWestRoom(Room west)
+	{
+		this.west = west;
+	}
+	void setSouthRoom(Room south)
+	{
+		this.south = south;
+	}
+	int[] getLocationbyRoomId(int roomid)
+	{
+		int[] coordinate = new int[2];
+		coordinate[0] = this.locationx;
+		coordinate[1] = this.locationy;
+		return coordinate;
+	}
+	int getRoomIdbyLocation(int locationx, int locationy)
+	{
+		int returnedID;
+		if(this.locationx == locationx && this.locationy == locationy)
+		{
+			returnedID = this.roomId;
+		}
+		else
+		{
+			returnedID = -1;
+		}
+		return returnedID;
+	}
+	void changeFloorColor()
+	{
+		floorCol = Color.MAGENTA;
+	}
+	
+	
+	
+	
+	
+	int getLocx()
+	{
+		return locationx;
+	}
+	int getLocy()
+	{
+		return locationy;
+	}
+	int roomId()
+	{
+		return roomId;
+	}
+	Room getNorthRoom()
+	{
+		return north;
+	}
+	Room getEastRoom()
+	{
+		return east;
+	}
+	Room getSouthRoom()
+	{
+		return south;
+	}
+	Room getWestRoom()
+	{
+		return west;
+	}
+	
 }
